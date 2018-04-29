dfd7d9a99dd284988435258a93e261aa94353c1f
Cyn <405835292@qq.com>
2018-04-30 01:53:07 +0800

    设置rem布局

--- src/project/components/menu/menu.vue
+++ src/project/components/menu/menu.vue
@@ -1,7 +1,7 @@
 <template>
     <ul>
         <li v-for="obj in menus" :key="obj.path">
-            <router-link :to="obj.path">{{obj.text}}</router-link>
+            <router-link :to="obj.path"><img :src="obj.src">{{obj.text}}</router-link>
         </li>
     </ul>
 </template>
@@ -11,11 +11,11 @@
         data(){
             return {
                 menus: [
-                    {path: '/home', text: '首页'},
-                    {path: '/goodslist', text: '菜单'},
-                    {path: '/order', text: '订单'},
-                    {path: '/shoppingcar', text: '购物车'},
-                    {path: '/user', text: '用户中心'},
+                    {path: '/home', text: '首页',src:'src/project/img/home.png'},
+                    {path: '/goodslist', text: '菜单',src:'src/project/img/menu.png'},
+                    {path: '/order', text: '订单',src:'src/project/img/order.png'},
+                    {path: '/shoppingcar', text: '购物车',src:'src/project/img/shopping_cart.png'},
+                    {path: '/user', text: '我的',src:'src/project/img/my.png'},
                 ]
             }
         }
